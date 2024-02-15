500880ecbdb745a8d025c1292b7ebd0f44038e50
neftsv <svkush2013@gmail.com>
2024-02-15 20:02:10 +0200

    Modify Carts.cs

--- InternetShop/InternetShop/Models/Carts.cs
+++ InternetShop/InternetShop/Models/Carts.cs
@@ -4,7 +4,5 @@
     {
         public int Id { get; set; }
         public int UserId {  get; set; }
-        public int ProductId {  get; set; }
-        public int Quantity {  get; set; }
     }
 }
