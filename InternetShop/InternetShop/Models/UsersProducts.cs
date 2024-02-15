e64bfaf5bc8bf9b69adbeea6883c854d89d8e88e
neftsv <svkush2013@gmail.com>
2024-02-15 18:33:24 +0200

    Add model UsersProducts.cs

---
+++ InternetShop/InternetShop/Models/UsersProducts.cs
@@ -0,0 +1,9 @@
+﻿namespace InternetShop.Models
+{
+    public class UsersProducts
+    {
+        public int Id { get; set; }
+        public int UserId {  get; set; }
+        public int ProductId { get; set; }
+    }
+}
