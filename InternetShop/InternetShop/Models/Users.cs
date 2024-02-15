977faaf8241bc2d3c6d296309fe2abae255d6580
neftsv <svkush2013@gmail.com>
2024-02-15 18:29:49 +0200

    Add model Users.cs

---
+++ InternetShop/InternetShop/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace InternetShop.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Surname {  get; set; }
+        public string? Name { get; set; }
+        public string? Patronimic {  get; set; }
+        public string? PhoneNumber {  get; set; }
+    }
+}
