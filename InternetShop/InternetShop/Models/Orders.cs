0f2df13aea26849951b0f5c57b61431cca099480
Sviatoslav Kushnir <35654531+neftsv@users.noreply.github.com>
2024-02-15 20:52:14 +0200

    Update Orders.cs

--- InternetShop/InternetShop/Models/Orders.cs
+++ InternetShop/InternetShop/Models/Orders.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId {  get; set; }
-        public List<CartsProducts>? CartProducts {  get; set; }
+        public List<CartsProducts> CartProducts {  get; set; }
         public decimal TotalPrice {  get; set; }
         public DateTime Date {  get; set; }
     }
