654031ca540dd6a50dd65ce17a3db84f7359a8fe
neftsv <svkush2013@gmail.com>
2024-02-15 20:08:02 +0200

    Modify Orders.cs. Add CartProducts list, remove Quantity property

--- InternetShop/InternetShop/Models/Orders.cs
+++ InternetShop/InternetShop/Models/Orders.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId {  get; set; }
-        public int ProductId {  get; set; }
-        public int Quantity {  get; set; }
+        public List<int>? CartProductsId {  get; set; }
         public decimal TotalPrice {  get; set; }
         public DateTime Date {  get; set; }
     }
