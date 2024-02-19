31dc25106194a1765793e6478481716ae35dae13
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-02-19 15:17:16 +0200

    Create AccountController.cs

---
+++ InternetShop/InternetShop/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using InternetShop.Models;
+using InternetShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternetShop.Controllers;
+
+public class AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager) : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            //login
+            var result = await signInManager.PasswordSignInAsync(model.Email!, model.Password!, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+        return View(model);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToAction("Index","Home");
+    }
+
+    public IActionResult Register()
+    {
+	    throw new NotImplementedException();
+    }
+}
