c59f2a95c56558e7a2e5b83913459c0b594be19b
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-02-19 15:11:17 +0200

    Create AppDbContext.cs

---
+++ InternetShop/InternetShop/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InternetShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetShop.Data;
+
+public class AppDbContext : IdentityDbContext<Users>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
