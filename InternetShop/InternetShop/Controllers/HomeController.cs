f6c5276292a838662bdfc08b7403e2d21f6c0c6a
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-02-19 15:16:46 +0200

    Update HomeController.cs

--- InternetShop/InternetShop/Controllers/HomeController.cs
+++ InternetShop/InternetShop/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using InternetShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace InternetShop.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
