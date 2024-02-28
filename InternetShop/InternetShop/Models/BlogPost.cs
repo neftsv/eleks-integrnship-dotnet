7375fa017af1300c6bc8cfc9445e5aad6aa71133
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-02-28 22:51:25 +0200

    Create BlogPost.cs

---
+++ InternetShop/InternetShop/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace InternetShop.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Author { get; set; }
+
+    }
+}
