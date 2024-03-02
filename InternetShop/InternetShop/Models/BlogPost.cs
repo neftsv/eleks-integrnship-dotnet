85f64c9e5324584330d9a5ba0f3c3376186e791d
IhorPetrunchak <ihorpetrunchak@gmail.com>
2024-03-02 02:55:18 +0200

    Update BlogPost.cs
    Update for work with userdashboard(AuthorId)

--- InternetShop/InternetShop/Models/BlogPost.cs
+++ InternetShop/InternetShop/Models/BlogPost.cs
@@ -3,6 +3,7 @@
     public class BlogPost
     {
         public int Id { get; set; }
+        public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
