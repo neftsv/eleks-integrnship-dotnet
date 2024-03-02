df9407e42d1d7c38e1e4ac6d929e3ac17cdb1a3c
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-03-02 20:04:09 +0200

    Create BlogPostVM.cs

---
+++ InternetShop/InternetShop/BlogPostVM.cs
@@ -0,0 +1,12 @@
+﻿namespace InternetShop
+{
+    public class BlogPostVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Author { get; set; }
+        public IFormFile photo {get; set; }
+}
+}
