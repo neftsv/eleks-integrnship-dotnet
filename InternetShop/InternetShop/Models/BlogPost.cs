08d79a6f7ccb85f031aa88732f105e89e601704d
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-03-05 20:54:27 +0200

    Update BlogPost.cs

--- InternetShop/InternetShop/Models/BlogPost.cs
+++ InternetShop/InternetShop/Models/BlogPost.cs
@@ -1,12 +1,15 @@
-﻿namespace InternetShop.Models
+﻿using Microsoft.Build.Framework;
+
+namespace InternetShop.Models
 {
-    public class BlogPost
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string Author { get; set; }
-        public string image { get; set; }
-    }
+	public class BlogPost
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public string Author { get; set; }
+		[Required]
+		public string image { get; set; }
+	}
 }
