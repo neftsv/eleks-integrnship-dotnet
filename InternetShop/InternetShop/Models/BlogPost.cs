bbecb16c5c5860637b888e01b52a8d2a0c8d4bce
KishchukV <158174697+KishchukV@users.noreply.github.com>
2024-03-06 19:05:58 +0200

    Update BlogPost.cs

--- InternetShop/InternetShop/Models/BlogPost.cs
+++ InternetShop/InternetShop/Models/BlogPost.cs
@@ -1,13 +1,20 @@
-﻿namespace InternetShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternetShop.Models
 {
     public class BlogPost
     {
         public int Id { get; set; }
-        public int AuthorId { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")] 
+        public Users Users { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         public string Author { get; set; }
+        [Required]
+        public string? image { get; set; }
 
     }
 }
